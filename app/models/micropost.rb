4578563e883279b244b07d8a2aa0c019cd3a7bdd
Chester Asuncion <asuncionmcv@gmail.com>
2022-02-23 19:56:42 +0800

    add validations to models

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -1,3 +1,4 @@
 class Micropost < ApplicationRecord
   belongs_to :user
+  validates :content, lenght: { maximum: 140 }, presence: true
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,5 @@
 class User < ApplicationRecord
+  # require all users to have a unique username and email
+  has_many :microposts
+  validates :name, :email, uniqueness: true, presence: true 
 end
