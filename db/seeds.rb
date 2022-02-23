d3831e8db1cc9a0dabed0df8939150b866da332f
Chester Asuncion <asuncionmcv@gmail.com>
2022-02-23 20:39:54 +0800

    Finish toy app
    fix microposts#index action

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,26 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+users = [
+  { name: 'ruby', email: 'ruby@reddit.com' },
+  { name: 'thruput', email: 'bad_thruput27@email.com' },
+  { name: 'rabbit', email: 'goodrabbit@email.com' },
+]
+
+users.each do |user|
+  User.create( name: user[:name], email: user[:email])
+end
+
+microposts = [
+  { content: 'rabbits' },
+  { content: 'new micropost 27' },
+  { content: 'new micropost 41' }
+]
+
+User.all.zip(microposts).each do |user_and_post|
+  user = user_and_post.first
+  micropost = user_and_post.last
+
+  user.microposts.create(content: micropost[:content])
+end
